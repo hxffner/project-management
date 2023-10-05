cd699b56479fcf89f82f0e56c3aff8355d8b6848
sromeo <strieglromeo2@gmail.com>
2023-10-06 00:31:17 +0200

    Package name typo:)

--- server/src/main/java/server/controllers/UserController.java
+++ server/src/main/java/server/controllers/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import server.models.User;
-import server.repositroy.UserRepository;
+import server.repository.UserRepository;
 
 import java.util.List;
 import java.util.Optional;
--- server/src/main/java/server/repository/UserRepository.java
+++ server/src/main/java/server/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package server.repositroy;
+package server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
