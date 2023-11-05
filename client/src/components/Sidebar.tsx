fe7c2d05920d1ebc3cc37e60b7bd3f5d2377d336
balinthegyi <90141157+balinthegyi@users.noreply.github.com>
2023-11-05 12:55:51 +0100

    create login and register button, create login rules

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC } from "react";
 
 import { Link } from "react-router-dom";
 
@@ -16,7 +16,10 @@ const Sidebar: FC = () => {
         <div className="flex-1">
           <ul className="pt-2 pb-4 space-y-1 text-sm">
             <li className="rounded-sm">
-              <Link to={`/calendar`} className="btn btn-ghost normal-case text-lg">
+              <Link
+                to={`/calendar`}
+                className="btn btn-ghost normal-case text-lg"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -81,6 +84,50 @@ const Sidebar: FC = () => {
                     />
                   </svg>
                   <span>Profile</span>
+                </Link>
+              </li>
+              <li className="rounded-sm">
+                <Link
+                  to={`/login`}
+                  className="btn btn-ghost normal-case text-lg"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+                    />
+                  </svg>
+                  <span>Login</span>
+                </Link>
+              </li>
+              <li className="rounded-sm">
+                <Link
+                  to={`/register`}
+                  className="btn btn-ghost normal-case text-lg"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+                    />
+                  </svg>
+                  <span>Register</span>
                 </Link>
               </li>
               <li className="rounded-sm">
--- client/src/pages/Auth/LoginPage.tsx
+++ client/src/pages/Auth/LoginPage.tsx
@@ -1,24 +1,69 @@
-import { FC } from 'react'
+import { ChangeEvent, FC, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface FormValues {
+  user: string;
+  pwd: string;
+}
 
 const LoginPage: FC = () => {
+  const [values, setValues] = useState<FormValues>({ user: "", pwd: "" });
+  const navigate = useNavigate();
+
+  const login = (user: string): void => {
+    console.log("succesfull login: " + user);
+    navigate("/calendar");
+  };
+
+  const handleSubmit = (event: React.FormEvent): void => {
+    event.preventDefault();
+
+    if (values.pwd !== "pwd1" && values.user !== "user1") {
+      alert("Hibás jelszó vagy felhasználónév!");
+      return;
+    }
+
+    login(values.user);
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
   return (
     <div className="mt-24">
-      <form className="flex flex-col items-center gap-2">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center gap-2"
+      >
         <span className="text-2xl font-semibold	 mb-2">Login Form</span>
         <input
           type="text"
           placeholder="Username"
           className="input input-bordered w-full max-w-md"
+          id="user"
+          name="user"
+          value={values.user}
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           className="input input-bordered w-full max-w-md"
+          id="password"
+          name="pwd"
+          value={values.pwd}
+          onChange={handleChange}
         />
-        <button type="submit" className="btn ">Login</button>
+        <button type="submit" className="btn ">
+          Login
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
