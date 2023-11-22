8ed72dde57f806f529fafe13303740662d7d3206
hxffner <hofi.marcell@gmail.com>
2023-11-22 10:52:57 +0100

    Logout Button

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC } from "react";
 
 import { Link } from "react-router-dom";
 
@@ -17,7 +17,10 @@ const Sidebar: FC = () => {
         <div className="flex-1">
           <ul className="pt-2 pb-4 space-y-1 text-sm">
             <li className="rounded-sm">
-              <Link to={`/calendar`} className="btn btn-ghost normal-case text-lg">
+              <Link
+                to={`/calendar`}
+                className="btn btn-ghost normal-case text-lg"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -66,7 +69,10 @@ const Sidebar: FC = () => {
           <div>
             <ul className="mb-4">
               <li className="rounded-sm">
-                <Link to={`/profile/${username}`} className="btn btn-ghost normal-case text-lg">
+                <Link
+                  to={`/profile/${username}`}
+                  className="btn btn-ghost normal-case text-lg"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -85,7 +91,10 @@ const Sidebar: FC = () => {
                 </Link>
               </li>
               <li className="rounded-sm">
-                <Link to={`/profile/${username}/settings`} className="btn btn-ghost normal-case text-lg">
+                <Link
+                  to={`/profile/${username}/settings`}
+                  className="btn btn-ghost normal-case text-lg"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -106,6 +115,26 @@ const Sidebar: FC = () => {
                     />
                   </svg>
                   <span>Settings</span>
+                </Link>
+              </li>
+              <li className="rounded-sm">
+                <Link to={`/`} className="btn btn-ghost normal-case text-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+                    />
+                  </svg>
+
+                  <span>Logout</span>
                 </Link>
               </li>
             </ul>
