2b57c89742766b8cea559f948df3b95e46bf82ae
hxffner <hofi.marcell@gmail.com>
2023-11-30 12:07:19 +0100

    Fixed bug with registration

--- client/src/pages/Profile/ProfilePage.tsx
+++ client/src/pages/Profile/ProfilePage.tsx
@@ -1,6 +1,11 @@
 import { FC } from "react";
+import { selectUser } from "../../features/auth/authSlice";
+import { useAppSelector } from "../../app/hooks";
+import dayjs from "dayjs";
 
 const ProfilePage: FC = () => {
+  const user = useAppSelector(selectUser);
+
   return (
     <div className="flex items-center flex-col">
       <div className="avatar">
@@ -8,7 +13,8 @@ const ProfilePage: FC = () => {
           <img src="https://e3.365dm.com/18/03/736x414/skynews-tony-soprano-james-gandolfini_4253599.jpg?20190110140558" />
         </div>
       </div>
-      <h2 className="ml-4 text-lg font-semibold">Tony Soprano</h2>
+      <h2 className="ml-4 text-lg font-semibold">{user?.username}</h2>
+      <p>User since {dayjs(user?.createdAt).format('MMMM YYYY')}</p>
     </div>
   );
 };
