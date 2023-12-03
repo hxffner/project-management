0265ab2687097380d3a2e00043c99ea29e92319f
Striegl Rómeó <72932972+manta0709@users.noreply.github.com>
2023-12-03 13:37:31 +0100

    Users can upload pictures (#21)
    These pictures then can be seen at:
    http://localhost:8080/images/{username}.jpg
    If you want to demo/use this feature first replace the value of pathString variable in UserController to fit your file system. (sorry couldn't figure out how to make it relative to base dir uwu, will see into it)

--- server/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users(username, first_name, last_name, email, password, avatar_path) VALUES ('user1', '', '', 'user1@mail.com', 'pwd1', '');
-INSERT INTO users(username, first_name, last_name, email, password, avatar_path) VALUES ('user2', '', '', 'user2@mail.com', 'pwd2', '');
-INSERT INTO users(username, first_name, last_name, email, password, avatar_path) VALUES ('user3', '', '', 'user3@mail.com', 'pwd3', '');
