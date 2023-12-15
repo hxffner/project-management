05c9160de8da6bcdff7ff8f8b570fd5155f10f31
sromeo <strieglromeo2@gmail.com>
2023-12-15 16:33:30 +0100

    Fájlfeltöltés demo. A koncepció az lenne, hogy a taskokhoz tartozik egy fájlreferencia lista, onnan csak a fájlok nevét lehet feltűntetni, meg lehet tudni a fájl id-t (így van tárolva). Listázott fájlra rányomva lehet szerverről lekérni azt az id alapján. Törlésnél meg hozzáadásnál kell a taskId is a referencia beállításához. Meg izé át kell írni megint egy path változót a CalendarEntryController-ben "pathString" név alatt, hogy rendesen működjön saját gépen. Remélem nem fog baszakodni semmit, de gyanítom, hogy a java-s File típus nem fog tetszeni neki Responsenak a get-nél. Aztán lehet mégis jó ki tudja:D (nem teszteltem még, sok sikert, godspeed!)

---
+++ server/src/main/java/server/payload/response/FileResponse.java
@@ -0,0 +1,30 @@
+package server.payload.response;
+
+import java.io.File;
+
+public class FileResponse {
+    private String name;
+
+    private File file;
+
+    public FileResponse(String name, File file) {
+        this.name = name;
+        this.file = file;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+}
---
+++ server/src/main/java/server/repository/FileRepository.java
@@ -0,0 +1,14 @@
+package server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import server.models.UploadedFile;
+
+import java.util.Optional;
+
+public interface FileRepository extends JpaRepository<UploadedFile, Long> {
+
+    Optional<UploadedFile> getUploadFileById(Long id);
+
+    Optional<UploadedFile> getUploadedFileByFilename(String filename);
+    UploadedFile save(UploadedFile uploadedFile);
+}
